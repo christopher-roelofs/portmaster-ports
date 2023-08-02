1b2a55cfafb89e308871a27cc0d28e4ecaa9039c
unknown <christopher.roelofs@gmail.com>
2023-08-02 19:03:11 -0400

    revert ports.json add counts.json

---
+++ get_releases.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+assets = {}
+
+resposne = requests.get("https://api.github.com/repos/PortsMaster/PortMaster-Releases/releases")
+for item in json.loads(resposne.text):
+    for asset in item["assets"]:
+        if ".zip" in asset["name"] and ".md5" not in asset["name"] and "images.zip" not in asset["name"] :
+            if asset["name"] not in assets:
+                assets[asset["name"]] = int(asset["download_count"])
+            else:
+                assets[asset["name"]] += int(asset["download_count"])
+
+print(json.dumps(assets))
