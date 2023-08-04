27aa3e74f9b1c12c9ec5ab169ebe431c3b9deae6
unknown <christopher.roelofs@gmail.com>
2023-08-04 08:04:22 -0400

    hook up porter page

--- get_releases.py
+++ get_releases.py
@@ -1,15 +1,20 @@
 import requests
 import json
+import re
+
+def name_cleaner(text):
+    temp = re.sub(r'[^a-zA-Z0-9 _\-\.]+', '', text.strip().casefold())
+    return re.sub(r'[ \.]+', '.', temp)
 
 assets = {}
 
 resposne = requests.get("https://api.github.com/repos/PortsMaster/PortMaster-Releases/releases")
 for item in json.loads(resposne.text):
     for asset in item["assets"]:
-        if ".zip" in asset["name"] and ".md5" not in asset["name"] and "images.zip" not in asset["name"] :
-            if asset["name"] not in assets:
-                assets[asset["name"]] = int(asset["download_count"])
+        if ".zip" in name_cleaner(asset["name"]) and ".md5" not in name_cleaner(asset["name"]) and "images.zip" not in name_cleaner(asset["name"]):
+            if name_cleaner(asset["name"]) not in assets:
+                assets[name_cleaner(asset["name"])] = int(asset["download_count"])
             else:
-                assets[asset["name"]] += int(asset["download_count"])
+                assets[name_cleaner(asset["name"])] += int(asset["download_count"])
 
-print(assets)
+print(json.dumps(assets))
